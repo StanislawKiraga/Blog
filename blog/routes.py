d3dfb2aee4043e3a79dee02fb559651dfe4e7577
StanislawKiraga <kiragasta@gmail.com>
2023-05-17 12:34:18 +0200

    add edit_entry

--- blog/routes.py
+++ blog/routes.py
@@ -1,11 +1,48 @@
-from flask import Flask, request, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from blog import app, db
-from blog.models import Entry
+from flask import Flask, request, redirect, render_template, url_for, flash
+from blog import app
+from blog.models import Entry, db
+from blog.forms import EntryForm
+
 
 @app.route("/")
 def index():
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    all_posts = Entry.query.filter_by(
+        is_published=True).order_by(Entry.pub_date.desc())
 
     return render_template("homepage.html", all_posts=all_posts)
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    form = EntryForm()
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+            db.session.commit()
+            flash('Wpis został dodany!')
+            return redirect(url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route('/edit-post/<int:entry_id>', methods=['GET', 'POST'])
+def edit_entry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form.populate_obj(entry)
+            db.session.commit()
+            flash('Wpis został zaktualizowany!')
+            return redirect(url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
 
