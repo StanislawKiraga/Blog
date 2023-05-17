2b76dde8e38d32abc0985163fe720d82e6124760
StanislawKiraga <kiragasta@gmail.com>
2023-05-17 10:35:45 +0200

    add posts

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from blog import app, db
+from blog.models import Entry
 
+@app.route("/")
+def index():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
 
-@app.route('/')
-def home():
-    return render_template('base.html')
+    return render_template("homepage.html", all_posts=all_posts)
+
