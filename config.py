e13170a83517dbba099f0804f0baf4c7794bffee
StanislawKiraga <kiragasta@gmail.com>
2023-05-17 13:33:46 +0200

    add login config

--- config.py
+++ config.py
@@ -10,4 +10,7 @@ class Config:
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+   ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
+   ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'password')
+
     
