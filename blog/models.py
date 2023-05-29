ac20812819622b861f91291e890c868d95ab43a0
StanislawKiraga <kiragasta@gmail.com>
2023-05-29 14:07:11 +0200

    create comment model

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,10 @@ class Entry(db.Model):
     body = db.Column(db.Text, nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     is_published = db.Column(db.Boolean, default=False)
+    comments = db.relationship('Comment', backref='entry', lazy=True)
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    entry_id = db.Column(db.Integer, db.ForeignKey('entry.id'), nullable=False)
